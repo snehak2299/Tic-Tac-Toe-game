41b45444f39b28dac6a6aca7ad927d73582ce3fe
snehak2299 <skanase2299@gmail.com>
2021-08-28 12:10:35 +0530

    [sneha]:ask user to select symbol

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,11 +1,23 @@
 package com.brideglab.tictactoe;
 
-public class TicTacToeGame {
-	char[] boardArr = new char[10]; 
+import java.util.Scanner;
+
+public class TicTacToeGame { 
 	
-	public void ticTacToeBoard() {
+	public static char[] ticTacToeBoard() {  //UC1
+		char[] boardArr = new char[10];
 		for(int i=1;i<boardArr.length;i++) {
 			boardArr[i]=' ';
 		}
+		return boardArr;
+		
 	}
+	
+	public char selectSymbol(){
+        System.out.print("select the symbol X or 0");
+        Scanner sc = new Scanner(System.in);
+        char symbolP= sc.next().charAt(0);
+        return symbolP ;
+    }
+	
 }
---
+++ TicTacToeMain.java
@@ -0,0 +1,21 @@
+package com.brideglab.tictactoe;
+
+public class TicTacToeMain {
+
+	public static void main(String[] args) {
+		TicTacToeGame ticTacToe = new TicTacToeGame();
+		ticTacToe.ticTacToeBoard();
+		char userSelect = ticTacToe.selectSymbol();
+		char symbolCom;
+        if(userSelect =='X') {
+        	symbolCom='O';
+        }
+        else {
+        	symbolCom='X';
+        }
+        System.out.println("computer symbol"+ symbolCom);
+    
+
+	}
+
+}
